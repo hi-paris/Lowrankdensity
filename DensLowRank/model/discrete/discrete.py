2ca6640c58892530e62b0e5949eff55b3120127d
Laurène DAVID <laurene.david@ip-paris.fr>
2023-04-13 15:21:43 +0200

    update models

--- DensLowRank/model/discrete/discrete.py
+++ DensLowRank/model/discrete/discrete.py
@@ -18,63 +18,20 @@ class Discrete:
     alpha : float, default 0.1
     Level of precision of matrix estimation
 
-    """
+    
+    Attributes
+    ----------
+    probability_matrix : np.ndarray of shape (d1,d2) with d1 : nbr of attributes of X1, d2 : nbr of attributes of X2
+    Estimated probability matrix of the joint distribution of the data X=(X1,X2)
 
-    Cbar = 1
+    """
 
     def __init__(self,alpha=0.1):
         self.alpha = alpha 
-        self.P = None
-
-        
-
-    def _compute_histograms(self,X):
-
-        """
-        Compute two histograms from dataset with two categorical/discrete variables.
-        Each histogram is a two-way frequency table of both variables's attributes
-        and is computed with one half of the data. 
-
-        return : 
-        ----
-        Y1: nd.array
-        A histogram of the first half of the data.
-
-        Y2: nd.array
-        A histogram of the second half of the data.
-
-        """
-
-        # create histogram with the first half of the data
-        data_1 = X[:int(len(X)/2),:]
-        _, Y1 = crosstab(data_1[:,0], data_1[:,1])
-
-        # create histogram with the second half of the data
-        data_2 = X[int(len(X)/2):,:]
-        _, Y2 = crosstab(data_2[:,0], data_2[:,1])
-
-        return Y1, Y2
-    
+        self.probability_matrix = None
 
 
     def _compute_matrix(self,n=None,Y1=None,Y2=None):
-
-        '''
-        Compute an matrix estimation of the joint multinomial probability of categorical data with two varibales.
-
-        Parameters:
-        ----
-
-        X: nd.array of size (nb_samples,2) 
-        A numpy array with 2 categorical columns (int, float or str)
-
-        
-        return:
-        ----
-        res/np.sum(res): numpy.ndarray
-        The estimated probability matrix of a multinomial distribution
-
-        '''
         # Normalize histograms
         Y1 = Y1/np.sum(Y1)
         Y2 = Y2/np.sum(Y2)
@@ -82,7 +39,7 @@ class Discrete:
         # Model constants 
         d = np.max(np.shape(Y1))
         cstar = self.alpha/10 
-        Cbar = self.Cbar
+        Cbar = 1
 
         # Case 1
         if (n <= d * np.log(d)):
@@ -143,19 +100,18 @@ class Discrete:
         ----
 
         X: nd.array of size (nb_samples,2) 
-        A numpy array with 2 categorical columns (int, float or str)
+        A numpy array with 2 categorical variables (int, float or str)
 
         
         return:
         ----
-        res/np.sum(res): numpy.ndarray
-        A probability matrix estimator of the data's multinomial distribution
+        self : object
+        Returns the instance itself.
 
         '''
 
 
-        # Seperate case where discrete function used in continuous function
-
+        # Cases where discrete model used in continuous function
         if discrete_case == True:
             if not isinstance(X, np.ndarray):
                 raise TypeError(f"Input X should be a nd.array, not a {type(X)}")
@@ -171,70 +127,49 @@ class Discrete:
         
             if type(self.alpha) not in (int,float):
                 raise ValueError(f"alpha should an int or float, not {type(self.alpha)}")
-                
-            Y1, Y2 = self._compute_histograms(X)
-            self.P = self._compute_matrix(n=X.shape[0],Y1=Y1,Y2=Y2)
+            
+            # create histogram with the first half of the data
+            Y1 = crosstab(X[:int(len(X)/2),0], X[:int(len(X)/2),1])[1]
+            Y2 = crosstab(X[int(len(X)/2):,0], X[int(len(X)/2):,1])[1]
+            self.probability_matrix = self._compute_matrix(n=X.shape[0],Y1=Y1,Y2=Y2)
         
         else:
-            self.P = self._compute_matrix()
+            self.probability_matrix = self._compute_matrix()
         
         return self
-    
-
-
-
-    def probability_matrix(self):
-        """
-        Class method to return Probability matrix 
-
-        Parameters 
-        -------   
-
-
-        Returns
-        -------
-        P: nd.array of shape (d1, d2)
-        
-        """
-        
-        P = self.P        
-        return P
-    
-
-    
-    def sample(self, n_samples=1, replace=True):
-        """
-        Sample discrete data with low_rank probability matrix P
-
-        Parameters 
-        -------   
-        n_samples : int, default=1
-        Number of samples to draw from distribution 
-
-        replace : bool, default=True
-        Sample from distribution with or without replacement
-
-        
-        Returns
-        -------
-        sample: nd.array of shape (n_samples,)
-        Samples drawn from discrete distribution with probability matrix P
-        
-
-        """
-        P = self.P
-        nrow, ncol = P.shape
-        p = np.reshape(P,nrow*ncol)
-        sample = np.random.choice(len(p), size=n_samples, p=p, replace=replace)
-        
-        return sample
-
-
-
-
 
 
     
+    # def sample(self, n_samples=1, replace=True):
+    #     """
+    #     Sample discrete data with low_rank probability matrix P
+
+    #     Parameters 
+    #     -------   
+    #     n_samples : int, default=1
+    #     Number of samples to draw from distribution 
+
+    #     replace : bool, default=True
+    #     Sample from distribution with or without replacement
+
+        
+    #     Returns
+    #     -------
+    #     sample: nd.array of shape (n_samples,)
+    #     Samples drawn from discrete distribution with probability matrix P
+        
+
+    #     """
+    #     P = self.probability_matrix
+    #     nrow, ncol = P.shape
+        
+    #     p = np.reshape(P,nrow*ncol)
+    #     sample = np.random.choice(len(p), size=n_samples, p=p, replace=replace)
+        
+    #     return sample
+
+
+
 
 
 ## Test of class discrete with dataset ##
@@ -246,7 +181,5 @@ class Discrete:
 
 # model = Discrete(alpha=0.01)
 # model.fit(X)
-
-# print(f"P: {model.probability_matrix()}")
-# print(f"sample: {model.sample(n_samples=10)}")
+# print(model.probability_matrix)
 
