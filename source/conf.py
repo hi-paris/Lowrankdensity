760d02be906db03ad1dffec2b2049d6068cc06c4
Gaëtan <gaetan.brison@gmail.com>
2023-05-15 15:22:45 +0200

    readme v1

---
+++ source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Lowrankdensity'
+copyright = '2023, Julien Chhor, Olga Klopp and Alexandre Tsybakov supported by Hi! PARIS Engineering Team (Laurène David, Shreshtha Shaurya and Gaëtan Brison)'
+author = 'Julien Chhor, Olga Klopp and Alexandre Tsybakov supported by Hi! PARIS Engineering Team (Laurène David, Shreshtha Shaurya and Gaëtan Brison)'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
